c1ab712639afa1cc37a13c2ce1b0014edb8f5637
iksiks30 <iksiks30@gmail.com>
2022-01-31 02:10:39 +0300

    Базовый класс Модель-Представление

---
+++ CV19/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CV19.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropetyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropetyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropetyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropetyName);
+            return true;
+        }
+    }
+}
