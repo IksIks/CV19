8be9bae397ac6ee5ff9e2e5d18e425503157429a
iksiks30 <iksiks30@gmail.com>
2022-02-01 17:28:41 +0300

    Тестирование работы ViewModel

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+
+        private string title = "Анализ статистики CV19";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => title;
+            set => Set(ref title, value);
+        } 
+
+        #endregion
+
+
+
+    }
+}
