5b3e4e5fe2f819406c3df7d80d670a8ca06894b8
iksiks30 <iksiks30@gmail.com>
2022-02-07 00:45:19 +0300

    Извлечение данных по дате записи

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19Console
 {
@@ -7,10 +13,39 @@ namespace CV19Console
     {
         private const string dataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv";
 
-        static void Main(string[] args)
+        private static async Task<Stream> GetDataStream()
         {
             var client = new HttpClient();
-            var csvStr = client.GetStringAsync(dataUrl).Result;
+            var response = await client.GetAsync(dataUrl, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var dataStream = GetDataStream().Result;
+            using var dataReader = new StreamReader(dataStream);
+            while(!dataReader.EndOfStream)
+            {
+                var line = dataReader.ReadLine();
+                if (String.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        private static DateTime[] GetDates() => GetDataLines()
+            .First()
+            .Split(',')
+            .Skip(4)
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+        static void Main(string[] args)
+        {
+            foreach (var item in GetDates())
+            {
+                Console.WriteLine(item);
+            }
+            
             Console.ReadLine();
         }
     }
