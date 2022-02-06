fdcf7ab88b20093d6c3ad905de4b44394ad2d782
iksiks30 <iksiks30@gmail.com>
2022-02-06 20:32:48 +0300

    Запрос к удаленному серверу на получение сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CV19Console
 {
     internal class Program
     {
+        private const string dataUrl = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+            var csvStr = client.GetStringAsync(dataUrl).Result;
+            Console.ReadLine();
         }
     }
 }
