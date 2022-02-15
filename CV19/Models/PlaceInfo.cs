3ee7d49b397b41c7f01bd042c9378eb9b6c71e60
iksiks30 <iksiks30@gmail.com>
2022-02-16 02:14:49 +0300

    Вывод на график тестовых данных

---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace CV19.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+    }
+
+    internal class CountryInfo : PlaceInfo
+    {
+        public IEnumerable<ProvinceInfo> ProvincesCount { get; set; }
+    }
+
+    internal class ProvinceInfo : PlaceInfo
+    {
+
+    }
+
+    internal struct ConfirmedCount
+    {
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+    }
+
+    internal struct DataPoint
+    {
+        public double XValue { get; set; }
+        public double YValue { get; set; }
+
+        public DataPoint(double x, double y)
+        {
+            XValue = x;
+            YValue = y;
+        }
+    }
+}
